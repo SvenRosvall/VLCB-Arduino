d07da3a222a3f4f404c0aa688dca4ae69beaa3e6
Sven Rosvall <sven.rosvall@cis.ntt.com>
2021-02-11 10:16:42 +0000

    Make the setProcessor() optional.

--- src/CBUSParams.h
+++ src/CBUSParams.h
@@ -1,3 +1,39 @@
+/*
+
+  Copyright (C) Sven Rosvall 2021
+
+  This work is licensed under the:
+      Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+   To view a copy of this license, visit:
+      http://creativecommons.org/licenses/by-nc-sa/4.0/
+   or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+   License summary:
+    You are free to:
+      Share, copy and redistribute the material in any medium or format
+      Adapt, remix, transform, and build upon the material
+
+    The licensor cannot revoke these freedoms as long as you follow the license terms.
+
+    Attribution : You must give appropriate credit, provide a link to the license,
+                  and indicate if changes were made. You may do so in any reasonable manner,
+                  but not in any way that suggests the licensor endorses you or your use.
+
+    NonCommercial : You may not use the material for commercial purposes. **(see note below)
+
+    ShareAlike : If you remix, transform, or build upon the material, you must distribute
+                 your contributions under the same license as the original.
+
+    No additional restrictions : You may not apply legal terms or technological measures that
+                                 legally restrict others from doing anything the license permits.
+
+   ** For commercial use, please contact the original copyright holder(s) to agree licensing terms
+
+    This software is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+
+*/
+
 #include <CBUSConfig.h>
 
 class CBUSParams
@@ -11,11 +47,17 @@ public:
     params[4] = config.EE_MAX_EVENTS;   //  4 num events
     params[5] = config.EE_NUM_EVS;      //  5 num evs per event
     params[6] = config.EE_NUM_NVS;      //  6 num NVs
+    params[9] = 0x32;
     params[10] = PB_CAN;
     params[11] = 0x00;
     params[12] = 0x00;
     params[13] = 0x00;
     params[14] = 0x00;
+    params[15] = '3';
+    params[16] = '2';
+    params[17] = '8';
+    params[18] = 'P';
+    params[19] = CPUM_ATMEL;
   }
 
   void setVersion(char major, char minor, char beta)
