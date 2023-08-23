875dc4f5395b5c7ebf12ab3ff33f32ee66c89eb3
Sven Rosvall <sven@rosvall.ie>
2023-08-23 09:30:23 +0100

    Rearrange mock objects so more can be used.

--- test/testMinimumNodeService.cpp
+++ test/testMinimumNodeService.cpp
@@ -2,8 +2,22 @@
 //  This file is part of VLCB-Arduino project on https://github.com/SvenRosvall/VLCB-Arduino
 //  Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 //  The full licence can be found at: http://creativecommons.org/licenses/by-nc-sa/4.0
-//
-//
+
+// Test cases for MinimumNodeService.
+// MNS implements these OP-codes.
+// * RQNN - (Initiated by module from user action.)
+// * NNREL - (Initiated by module from user action.)
+// * SNN - Done
+// * QNN - Done
+// * RQNP - Done
+// * RQMN - Done
+// * RQNPN - Done
+// * RDGN - No support in MNS yet
+// * RQSD - Done - Tests done
+// * MODE - No support in MNS yet
+// * SQU - ????
+// * NNRST - Done
+// * NNRSM - Done
 
 #include <memory>
 #include "TestTools.hpp"
@@ -19,12 +33,13 @@
 
 namespace
 {
+static std::unique_ptr<MockUserInterface> mockUserInterface;
+static std::unique_ptr<MockTransport> mockTransport;
 
-VLCB::Controller createController(MockTransport &mockTransport)
+VLCB::Controller createController()
 {
   // Use pointers to objects to create the controller with.
   // Use static unique_ptr so that next invocation deletes the previous objects.
-  static std::unique_ptr<MockUserInterface> mockUserInterface;
   mockUserInterface.reset(new MockUserInterface);
   static std::unique_ptr<MockStorage> mockStorage;
   mockStorage.reset(new MockStorage);
@@ -36,7 +51,7 @@ VLCB::Controller createController(MockTransport &mockTransport)
   longMessageService.reset(new VLCB::LongMessageService);
   static std::unique_ptr<VLCB::CbusService> cbusService;
   cbusService.reset(new VLCB::CbusService);
-  VLCB::Controller controller(mockUserInterface.get(), configuration.get(), &mockTransport,
+  VLCB::Controller controller(mockUserInterface.get(), configuration.get(), mockTransport.get(),
                               {minimumNodeService.get(), longMessageService.get(), cbusService.get()});
 
   configuration->EE_NVS_START = 0;
@@ -65,32 +80,31 @@ void testServiceDiscovery()
 {
   test();
 
-  MockTransport mockTransport;
-  VLCB::Controller controller = createController(mockTransport);
+  VLCB::Controller controller = createController();
 
   VLCB::CANFrame msg_rqsd = {0x11, false, false, 4, {OPC_RQSD, 0x01, 0x04, 0}};
-  mockTransport.setNextMessage(msg_rqsd);
+  mockTransport->setNextMessage(msg_rqsd);
 
   controller.process();
 
   // Verify sent messages.
-  assertEquals(4, mockTransport.sent_messages.size());
+  assertEquals(4, mockTransport->sent_messages.size());
 
-  assertEquals(OPC_SD, mockTransport.sent_messages[0].data[0]);
-  assertEquals(3, mockTransport.sent_messages[0].data[5]); // Number of services
+  assertEquals(OPC_SD, mockTransport->sent_messages[0].data[0]);
+  assertEquals(3, mockTransport->sent_messages[0].data[5]); // Number of services
 
-  assertEquals(OPC_SD, mockTransport.sent_messages[1].data[0]);
-  assertEquals(1, mockTransport.sent_messages[1].data[3]); // index
-  assertEquals(1, mockTransport.sent_messages[1].data[4]); // service ID
-  assertEquals(1, mockTransport.sent_messages[1].data[5]); // version
+  assertEquals(OPC_SD, mockTransport->sent_messages[1].data[0]);
+  assertEquals(1, mockTransport->sent_messages[1].data[3]); // index
+  assertEquals(1, mockTransport->sent_messages[1].data[4]); // service ID
+  assertEquals(1, mockTransport->sent_messages[1].data[5]); // version
 
-  assertEquals(OPC_SD, mockTransport.sent_messages[2].data[0]);
-  assertEquals(2, mockTransport.sent_messages[2].data[3]); // index
-  assertEquals(17, mockTransport.sent_messages[2].data[4]); // service ID
-  assertEquals(1, mockTransport.sent_messages[2].data[5]); // version
+  assertEquals(OPC_SD, mockTransport->sent_messages[2].data[0]);
+  assertEquals(2, mockTransport->sent_messages[2].data[3]); // index
+  assertEquals(17, mockTransport->sent_messages[2].data[4]); // service ID
+  assertEquals(1, mockTransport->sent_messages[2].data[5]); // version
 
-  assertEquals(OPC_SD, mockTransport.sent_messages[3].data[0]);
-  assertEquals(3, mockTransport.sent_messages[3].data[3]); // index
+  assertEquals(OPC_SD, mockTransport->sent_messages[3].data[0]);
+  assertEquals(3, mockTransport->sent_messages[3].data[3]); // index
   // Don't care about details of CbusService.
 }
 
@@ -98,19 +112,18 @@ void testServiceDiscoveryLongMessageSvc()
 {
   test();
 
-  MockTransport mockTransport;
-  VLCB::Controller controller = createController(mockTransport);
+  VLCB::Controller controller = createController();
 
   VLCB::CANFrame msg_rqsd = {0x11, false, false, 4, {OPC_RQSD, 0x01, 0x04, 2}};
-  mockTransport.setNextMessage(msg_rqsd);
+  mockTransport->setNextMessage(msg_rqsd);
 
   controller.process();
 
   // Verify sent messages.
-  assertEquals(1, mockTransport.sent_messages.size());
-  assertEquals(OPC_ESD, mockTransport.sent_messages[0].data[0]);
-  assertEquals(2, mockTransport.sent_messages[0].data[3]); // index
-  assertEquals(17, mockTransport.sent_messages[0].data[4]); // service ID
+  assertEquals(1, mockTransport->sent_messages.size());
+  assertEquals(OPC_ESD, mockTransport->sent_messages[0].data[0]);
+  assertEquals(2, mockTransport->sent_messages[0].data[3]); // index
+  assertEquals(17, mockTransport->sent_messages[0].data[4]); // service ID
   // Not testing service data bytes.
 }
 
@@ -118,20 +131,19 @@ void testServiceDiscoveryIndexOutOfBand()
 {
   test();
 
-  MockTransport mockTransport;
-  VLCB::Controller controller = createController(mockTransport);
+  VLCB::Controller controller = createController();
 
   VLCB::CANFrame msg_rqsd = {0x11, false, false, 4, {OPC_RQSD, 0x01, 0x04, 7}};
-  mockTransport.setNextMessage(msg_rqsd);
+  mockTransport->setNextMessage(msg_rqsd);
 
   controller.process();
 
   // Verify sent messages.
-  assertEquals(1, mockTransport.sent_messages.size());
-  assertEquals(OPC_GRSP, mockTransport.sent_messages[0].data[0]);
-  assertEquals(OPC_RQSD, mockTransport.sent_messages[0].data[3]);
-  assertEquals(1, mockTransport.sent_messages[0].data[4]); // service ID of MNS
-  assertEquals(CMDERR_INV_PARAM_IDX, mockTransport.sent_messages[0].data[5]); // result
+  assertEquals(1, mockTransport->sent_messages.size());
+  assertEquals(OPC_GRSP, mockTransport->sent_messages[0].data[0]);
+  assertEquals(OPC_RQSD, mockTransport->sent_messages[0].data[3]);
+  assertEquals(1, mockTransport->sent_messages[0].data[4]); // service ID of MNS
+  assertEquals(CMDERR_INV_PARAM_IDX, mockTransport->sent_messages[0].data[5]); // result
   // Not testing service data bytes.
 }
 
@@ -139,20 +151,19 @@ void testServiceDiscoveryShortMessage()
 {
   test();
 
-  MockTransport mockTransport;
-  VLCB::Controller controller = createController(mockTransport);
+  VLCB::Controller controller = createController();
 
   VLCB::CANFrame msg_rqsd = {0x11, false, false, 3, {OPC_RQSD, 0x01, 0x04}};
-  mockTransport.setNextMessage(msg_rqsd);
+  mockTransport->setNextMessage(msg_rqsd);
 
   controller.process();
 
   // Verify sent messages.
-  assertEquals(1, mockTransport.sent_messages.size());
-  assertEquals(OPC_GRSP, mockTransport.sent_messages[0].data[0]);
-  assertEquals(OPC_RQSD, mockTransport.sent_messages[0].data[3]);
-  assertEquals(1, mockTransport.sent_messages[0].data[4]); // service ID of MNS
-  assertEquals(CMDERR_INV_CMD, mockTransport.sent_messages[0].data[5]); // result
+  assertEquals(1, mockTransport->sent_messages.size());
+  assertEquals(OPC_GRSP, mockTransport->sent_messages[0].data[0]);
+  assertEquals(OPC_RQSD, mockTransport->sent_messages[0].data[3]);
+  assertEquals(1, mockTransport->sent_messages[0].data[4]); // service ID of MNS
+  assertEquals(CMDERR_INV_CMD, mockTransport->sent_messages[0].data[5]); // result
   // Not testing service data bytes.
 }
 
