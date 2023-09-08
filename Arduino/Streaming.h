8dba6f0be8eead739d908acda95c14745d21847c
Sven Rosvall <sven@rosvall.ie>
2023-09-08 21:11:01 +0100

    Provide implementation for _HEX() modifier for testing.

--- Arduino/Streaming.h
+++ Arduino/Streaming.h
@@ -20,4 +20,4 @@ Serial_T & operator<<(Serial_T &, unsigned long);
 Serial_T & operator<<(Serial_T &, const char *);
 Serial_T & operator<<(Serial_T & s, const ENDL_T & e);
 extern ENDL_T endl;
-template <typename T> T _HEX(T);
+template <typename T> T _HEX(T v) { return v;}
