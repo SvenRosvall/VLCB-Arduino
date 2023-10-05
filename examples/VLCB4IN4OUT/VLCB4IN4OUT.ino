eefa52636779c23f47d6e459a5d141ce2363c615
MartinDC <79657109+MartinDaCosta53@users.noreply.github.com>
2023-10-05 16:18:39 +0100

    Licence change and small presentational changes

--- examples/VLCB4IN4OUT/VLCB4IN4OUT.ino
+++ examples/VLCB4IN4OUT/VLCB4IN4OUT.ino
@@ -1,42 +1,18 @@
 
-// CANmINnOUT
+// VLCB4in4out
 
 
 /*
-  Copyright (C) 2021 Martin Da Costa
-  Including copyrights from VLCB_1in1out and Arduino VLCB Libraries
+  Copyright (C) 2023 Martin Da Costa
+  //  This file is part of VLCB-Arduino project on https://github.com/SvenRosvall/VLCB-Arduino
+//  Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+//  The full licence can be found at: http://creativecommons.org/licenses/by-nc-sa/4.0
 
+/*
+      3rd party libraries needed for compilation: (not for binary-only distributions)
 
-  This work is licensed under the:
-      Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
-   To view a copy of this license, visit:
-      http://creativecommons.org/licenses/by-nc-sa/4.0/
-   or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
-
-   License summary:
-    You are free to:
-      Share, copy and redistribute the material in any medium or format
-      Adapt, remix, transform, and build upon the material
-
-    The licensor cannot revoke these freedoms as long as you follow the license terms.
-
-    Attribution : You must give appropriate credit, provide a link to the license,
-                  and indicate if changes were made. You may do so in any reasonable manner,
-                  but not in any way that suggests the licensor endorses you or your use.
-
-    NonCommercial : You may not use the material for commercial purposes. **(see note below)
-
-    ShareAlike : If you remix, transform, or build upon the material, you must distribute
-                 your contributions under the same license as the original.
-
-    No additional restrictions : You may not apply legal terms or technological measures that
-                                 legally restrict others from doing anything the license permits.
-
-   ** For commercial use, please contact the original copyright holder(s) to agree licensing terms
-
-    This software is distributed in the hope that it will be useful, but WITHOUT ANY
-    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
-
+      Streaming   -- C++ stream style output, v5, (http://arduiniana.org/libraries/streaming/)
+      ACAN2515    -- library to support the MCP2515/25625 CAN controller IC
 */
 
 /*
@@ -306,17 +282,13 @@ void processSwitches(void) {
                         << (moduleSwitch[i].fell() ? F(" pressed, send state: ") : F(" released, send state: ")) << state);
             epService.sendEvent(state, i);
           }
-
           break;
 
         default:
           DEBUG_PRINT(F("> Invalid EV value."));
           break;
       }
-    }
-  }
-  if (!isSuccess) {
-    DEBUG_PRINT(F("> One of the send message events failed"));
+    } 
   }
 }
 
