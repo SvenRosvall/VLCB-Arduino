725bd5ddbf1eb12b118fe639a7de2b709ed2e3df
Chris Andrews <chris@nodnol.org>
2023-11-24 19:31:07 +0000

    Switch default compiler to clang

--- build-all.sh
+++ build-all.sh
@@ -4,7 +4,9 @@ set -xe
 # Tests
 cmake -S. -Bbuild-test \
       -D CMAKE_TOOLCHAIN_FILE=cmake/toolchain/host.toolchain.cmake \
-      -D CMAKE_BUILD_TYPE=MinSizeRel
+      -D CMAKE_C_COMPILER=clang \
+      -D CMAKE_CXX_COMPILER=clang++ \
+      -D CMAKE_BUILD_TYPE=Debug
 (cd build-test && make testAll && ctest)
 
 # AVR binary
