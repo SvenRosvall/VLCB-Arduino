b6d893f20b7ee1aab576afc80e5ec0aa553880dd
Sven Rosvall <sven@rosvall.ie>
2023-12-20 19:36:36 +0000

    Fix sending message when creating default produced event.

--- src/EventProducerService.h
+++ src/EventProducerService.h
@@ -41,7 +41,7 @@ private:
   void (*eventhandler)(byte index, VlcbMessage *msg);
 
   void setProducedEvents();
-  void createDefaultEvent(byte evValue);
+  byte createDefaultEvent(byte evValue);
   bool uninit = false;
 };
 
