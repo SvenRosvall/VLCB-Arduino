8fae03d58062c138fb065ec8eb60f5210404f130
Sven Rosvall <sven@rosvall.ie>
2024-03-19 12:01:07 +0000

    Fix typo.

--- src/EventProducerService.cpp
+++ src/EventProducerService.cpp
@@ -228,7 +228,7 @@ void EventProducerService::handleProdSvcMessage(const VlcbMessage *msg)
     switch (opc)
     {
       case OPC_ASRQ:
-        if ((nn != module_config->nodeNumber) || (nn != 0000))
+        if ((nn != module_config->nodeNum) || (nn != 0000))
         {
           return;
         }
