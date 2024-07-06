fc54e489b1e97fab0a758f2d3869b7a124bd5cd1
Sven Rosvall <sven@rosvall.ie>
2024-07-06 22:54:38 +0100

    Add include guard in initializer_list.h for use with CLang.

--- src/initializer_list.h
+++ src/initializer_list.h
@@ -3,9 +3,11 @@
 // Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 // The full licence can be found at: http://creativecommons.org/licenses/by-nc-sa/4.0/
 
-// Use the same include guard as G++ uses so that this doesn't clash for running the test suite.
-#ifndef _INITIALIZER_LIST
-#define _INITIALIZER_LIST
+// Use the same include guard as GNU C++ and Clang uses so that this doesn't clash for running the test suite.
+// Note the double include guard macros as GNU C++ and Clang use different include guard macros.
+#if !defined(_INITIALIZER_LIST) && !defined(_LIBCPP_INITIALIZER_LIST)
+#define _INITIALIZER_LIST        // GNU C++
+#define _LIBCPP_INITIALIZER_LIST // Clang
 
 #include <stddef.h>
 
