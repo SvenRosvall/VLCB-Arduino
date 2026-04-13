234f9da22387bca61241fc1d51211e8cb12c1887
Sven Rosvall <sven@rosvall.ie>
2026-04-13 10:10:48 +0100

    Add new doc files for Event Conuser and Teaching Services.

---
+++ html/search/pages_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['service_20api_0',['Service API',['../class_v_l_c_b_1_1_event_consumer_service.html#autotoc_md3',1,'Event Consumer Service API'],['../class_v_l_c_b_1_1_event_teaching_service.html#autotoc_md4',1,'Event Teaching Service API'],['../class_v_l_c_b_1_1_event_producer_service.html#autotoc_md2',1,'Producer Service API']]]
+];
---
+++ html/search/pages_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['teaching_20service_20api_0',['Event Teaching Service API',['../class_v_l_c_b_1_1_event_teaching_service.html#autotoc_md4',1,'']]]
+];
