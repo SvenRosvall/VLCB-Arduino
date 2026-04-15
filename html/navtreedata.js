9c79a268a06868781e29a8ac47df991737fc56c7
Sven Rosvall <sven@rosvall.ie>
2026-04-15 21:49:25 +0100

    Improve API documentation title.

--- html/navtreedata.js
+++ html/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "VLCB", "index.html", [
+  [ "Arduino C++ Library for VLCB", "index.html", [
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
       [ "Namespace Members", "namespacemembers.html", [
